eac06d0cc4c5fde3e1479f477ec03ff35aad8f0c
Sfzmango <maunghtike1999@gmail.com>
2025-07-11 03:21:26 -0700

    Fix asset paths for GitHub Pages with assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/mhrkwedding' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/mhrkwedding' : '',
 };
 
 export default nextConfig;
